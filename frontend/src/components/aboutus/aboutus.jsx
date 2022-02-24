a80a09c043f8a3170dc54d5763a97da3de9503da
ErikDz <erikdz.spy@gmail.com>
2022-02-24 19:50:56 +0100

    finiquitao?

--- frontend/src/components/aboutus/aboutus.jsx
+++ frontend/src/components/aboutus/aboutus.jsx
@@ -29,7 +29,7 @@ function AboutUs(){
                      />
                     <PersonCard image={Fryderyk}
                     name={"Fryderyk Sitnik"}
-                    title={"Co-Founder - CMO"}
+                    title={"Co-Founder - COO"}
                     description={"Student of Mathematics and Computer Science at École Polytechnique de Paris. Founder and President of the Board of Directors of the TeenCrunch Foundation, a non-profit startup accelerator for youth in Poland. Organized two batches of his accelerator with 9 startups in total."}
                     personalsite={""}
                      />
